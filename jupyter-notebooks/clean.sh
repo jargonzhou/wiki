5886b5ba385563f5b399517e054b2e6377cec87f
zhoujiagen <zhoujiagen@gmail.com>
2025-01-02 10:45:05 +0800

    Update wiki.

--- jupyter-notebooks/clean.sh
+++ jupyter-notebooks/clean.sh
@@ -8,12 +8,14 @@ find . -depth -name '__pycache__' -type d -print -exec rm -r {} +
 echo "Delete .venv..."
 find . -depth -name '.venv' -type d -print -exec rm -r {} + 
 
-echo "Delete common-lisp-jupyter"
-# find . -name 'kernel-*.log' -type f -delete
-find . -depth -name 'common-lisp-jupyter' -type d -print -exec rm -r {} + 
 
-echo "Delete alive fasl"
-find . -name '*.fasl' -type f -delete
-find . -name 'tmp.lisp' -type f -delete
+chmod +x CommonLisp/clean.sh
+CommonLisp/clean.sh
+
+chmod +x Scala/clean.sh
+Scala/clean.sh
+
+chmod +x Compiler/clean.sh
+Compiler/clean.sh
 
 echo "Done"
